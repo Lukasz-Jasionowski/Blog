d4e8f7309ae03af132c9b6a3d7b00ead8c4829e2
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-08-14 18:09:30 +0200

    Move header to Header.jsx, import and use Header Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
+import Header from './Components/Header';
 import Post from './Components/Post';
 
 function App() {
   return (
     <main>
-      <header>
-        <a href="" className="logo">My Blog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
+      <Header />
       <Post />
     </main>
   );
