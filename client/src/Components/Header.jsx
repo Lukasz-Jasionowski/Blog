495522ad818052e94abc00967b16533345e78dc1
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-08-14 18:09:04 +0200

    Create Header.jsx

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <header>
+                <a href="" className="logo">My Blog</a>
+                <nav>
+                    <a href="">Login</a>
+                    <a href="">Register</a>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default Header
