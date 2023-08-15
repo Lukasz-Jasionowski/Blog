a873f9ec9cb615f6b9e19dd9f442ad2664ece4e4
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-08-15 18:05:22 +0200

    Update RegisterPage Component with Backend Integration

--- client/src/Components/RegisterPage.jsx
+++ client/src/Components/RegisterPage.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function RegisterPage() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const serverURL = process.env.REACT_APP_SERVER_URL;
+    async function register(e) {
+        e.preventDefault();
+        await fetch(`${serverURL}/register`, {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' }
+        })
+    }
     return (
-        <form className='register'>
+        <form className='register' onSubmit={register}>
             <h1>Register</h1>
-            <input type='text' placeholder='username' />
-            <input type='password' placeholder='password' />
+            <input type='text'
+                placeholder='username'
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+            />
+            <input type='password'
+                placeholder='password'
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+            />
             <button>Register</button>
         </form>
     )
