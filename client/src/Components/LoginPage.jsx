528804f598e9534c7bf57591fa5ce8ff9c4e079a
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-08-15 09:21:45 +0200

    Update LoginPage Component to Include Login Form

--- client/src/Components/LoginPage.jsx
+++ client/src/Components/LoginPage.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function LoginPage() {
     return (
-        <div>Login Page</div>
+        <form className='login'>
+            <h1>Login</h1>
+            <input type='text' placeholder='username' />
+            <input type='password' placeholder='password' />
+            <button>Login</button>
+        </form>
     )
 }
 
