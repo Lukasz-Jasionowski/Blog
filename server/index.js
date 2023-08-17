18c924e683f670a619a4b0c0b219900db39d84a8
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-08-17 21:28:41 +0200

    Server configuration modification with MongoDB integration

--- server/index.js
+++ server/index.js
@@ -1,14 +1,25 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
 const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/register', (req, res) => {
+mongoose.connect(process.env.MONGODB_URL);
+
+app.post('/register', async (req, res) => {
     const { username, password } = req.body;
-    res.json({ requestData: { username, password } });
+    try {
+        const userDoc = await User.create({ username, password });
+        res.json(userDoc);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json(error);
+    }
 });
 
 app.listen(PORT, () => {
