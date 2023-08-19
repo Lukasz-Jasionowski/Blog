99c8f36616bc44c9bb3a18b0e5234db2a5690c93
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-08-19 21:47:35 +0200

    Update LoginPage Component with Backend Integration

--- client/src/Components/LoginPage.jsx
+++ client/src/Components/LoginPage.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 function LoginPage() {
+    const serverURL = process.env.REACT_APP_SERVER_URL;
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    async function login(e) {
+        e.preventDefault();
+        await fetch(`${serverURL}/login`, {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
     return (
-        <form className='login'>
+        <form className='login' onSubmit={login}>
             <h1>Login</h1>
-            <input type='text' placeholder='username' />
-            <input type='password' placeholder='password' />
+            <input type='text'
+                placeholder='username'
+                value={username}
+                onChange={e => setUsername(e.target.value)} />
+
+            <input type='password'
+                placeholder='password'
+                value={password}
+                onChange={e => setPassword(e.target.value)} />
             <button>Login</button>
         </form>
     )
