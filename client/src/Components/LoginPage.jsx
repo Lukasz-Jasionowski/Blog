0d3a8b9c1197392d1f49fc47b540942888278b91
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-08-20 21:44:25 +0200

    Redirect to home page when credentials match

--- client/src/Components/LoginPage.jsx
+++ client/src/Components/LoginPage.jsx
@@ -1,18 +1,31 @@
 import React, { useState } from 'react'
+import { Navigate } from 'react-router-dom'
 
 
 function LoginPage() {
     const serverURL = process.env.REACT_APP_SERVER_URL;
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [redirect, setRedirect] = useState(false);
     async function login(e) {
         e.preventDefault();
-        await fetch(`${serverURL}/login`, {
+        const response = await fetch(`${serverURL}/login`, {
             method: 'POST',
             body: JSON.stringify({ username, password }),
-            headers: { 'Content-Type': 'application/json' }
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include',
         });
+        if (response.ok) {
+            setRedirect(true);
+        } else {
+            alert('Wrong credentials!');
+        }
     }
+
+    if (redirect) {
+        return <Navigate to={'/'} />
+    }
+
     return (
         <form className='login' onSubmit={login}>
             <h1>Login</h1>
