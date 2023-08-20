81a2c995eeb3bc64652b5cd22b023449736d8412
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-08-20 21:43:36 +0200

    Add and use jsonwebtoken when credentials match

--- server/index.js
+++ server/index.js
@@ -6,9 +6,11 @@ const mongoose = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const salt = bcrypt.genSaltSync(10);
+const secret = process.env.SECRET;
+const jwt = require('jsonwebtoken');
 const PORT = process.env.PORT || 4000;
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URL);
@@ -32,9 +34,12 @@ app.post('/login', async (req, res) => {
     const userDoc = await User.findOne({ username: username });
     const passOk = bcrypt.compareSync(password, userDoc.password);
     if (passOk) {
-        // logged in
+        jwt.sign({ username, id: userDoc._id }, secret, {}, (error, token) => {
+            if (error) throw error;
+            res.cookie('token', token).json('Ok');
+        });
     } else {
-        re.status(400).json('Wrong credentials!');
+        res.status(400).json('Wrong credentials!');
     }
 });
 
