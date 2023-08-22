9da84df24e70960780452593223b5229310381d1
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-08-22 17:22:14 +0200

    Update Header Component to Fetch User Profile Information

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, json } from 'react-router-dom'
 
 function Header() {
+    const [username, setUsername] = useState(null);
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_SERVER_URL}/profile`, {
+            credentials: 'include'
+        }).then(response => {
+            response.json().then(userInfo => {
+                setUsername(userInfo.username);
+            });
+        });
+    }, []);
     return (
         <div>
             <header>
                 <Link to="/" className="logo">My Blog</Link>
                 <nav>
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
+                    {username && (
+                        <>
+                            <Link to={'/create'}>Create new post</Link>
+                        </>
+                    )}
+                    {!username && (
+                        <>
+                            <Link to="/login">Login</Link>
+                            <Link to="/register">Register</Link>
+                        </>
+                    )}
                 </nav>
             </header>
         </div>
