d5466999e0cea341bb445331360c4214467820a3
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-08-23 21:14:41 +0200

    Import and use UserContext

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,19 @@ import Layout from './Components/Layout';
 import LoginPage from './Components/LoginPage';
 import { Route, Routes } from 'react-router-dom'
 import RegisterPage from './Components/RegisterPage';
+import { UserContextProvider } from './Components/UserContext';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-      </Route>
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
   );
 }
 export default App;
