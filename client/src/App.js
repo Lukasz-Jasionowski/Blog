dd89eef70ff997962d5666383e10d4d00545e0bf
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-08-23 22:22:53 +0200

    Import and use CreatePost Component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import LoginPage from './Components/LoginPage';
 import { Route, Routes } from 'react-router-dom'
 import RegisterPage from './Components/RegisterPage';
 import { UserContextProvider } from './Components/UserContext';
+import CreatePost from './Components/CreatePost';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path='/login' element={<LoginPage />} />
           <Route path='/register' element={<RegisterPage />} />
+          <Route path='/create' element={<CreatePost />} />
         </Route>
       </Routes>
     </UserContextProvider>
