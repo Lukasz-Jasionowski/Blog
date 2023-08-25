5a8af4490bceb402a1c2870cd1ba2c02e8ec7ae5
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-08-25 21:31:12 +0200

    Update Home Component to Fetch and Display Posts from DB

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Post from './Post'
 
 function Home() {
+    const Server_URL = process.env.REACT_APP_SERVER_URL;
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch(`${Server_URL}/post`).then(response => {
+            response.json().then(posts => {
+                setPosts(posts);
+            });
+        });
+    }, []);
+
     return (
         <>
-            <Post />
+            {posts.length > 0 && posts.map(post => (
+                <Post {...post} />
+            ))}
         </>
-    )
+    );
 }
 
 export default Home
