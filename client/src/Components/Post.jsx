c5f0a1acf7e31b3ced959434615dd9cebb8b64a8
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-08-28 22:03:39 +0200

    Add functionality to navigate to specific post when clicking on the title or image

--- client/src/Components/Post.jsx
+++ client/src/Components/Post.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { formatISO9075 } from 'date-fns'
 
-function Post({ title, summary, cover, content, createdAt, author }) {
+function Post({ _id, title, summary, cover, content, createdAt, author }) {
     const Server_URL = process.env.REACT_APP_SERVER_URL;
     return (
         <div>
             <div className="post">
                 <div className="image">
-                    <img src={`${Server_URL}/${cover}`} alt="" />
+                    <Link to={`/post/${_id}`}>
+                        <img src={`${Server_URL}/${cover}`} alt="" />
+                    </Link>
                 </div>
                 <div className="texts">
-                    <h2>{title}</h2>
+                    <Link to={`/post/${_id}`}>
+                        <h2>{title}</h2>
+                    </Link>
                     <p className="info">
                         <a className="author">{author.username}</a>
                         <time>{formatISO9075(new Date(createdAt))}</time>
