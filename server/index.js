c655a30173cdb67a495c8225773336d75151fc4e
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-08-28 22:07:35 +0200

    Add route to fetch specific post by ID with author population

--- server/index.js
+++ server/index.js
@@ -94,7 +94,13 @@ app.get('/post', async (req, res) => {
         .sort({ createdAt: -1 })
         .limit(20)
     );
-})
+});
+
+app.get('/post/:id', async (req, res) => {
+    const { id } = req.params;
+    const postDoc = await Post.findById(id).populate('author', ['username']);
+    res.json(postDoc);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on Port ${PORT}`);
