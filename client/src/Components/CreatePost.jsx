c6eea62b8236113db5af9344bc20a01b2c25f1e8
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-08-31 21:01:52 +0200

    Import and use Editor Component instead ReactQuill to simplicity

--- client/src/Components/CreatePost.jsx
+++ client/src/Components/CreatePost.jsx
@@ -1,23 +1,7 @@
 import React, { useState } from 'react'
-import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 import { Navigate } from 'react-router-dom'
-
-const modules = {
-    toolbar: [
-        [{ 'header': [1, 2, false] }],
-        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-        [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
-        ['link', 'image'],
-        ['clean']
-    ],
-};
-const formats = [
-    'header',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet', 'indent',
-    'link', 'image'
-];
+import Editor from './Editor';
 
 function CreatePost() {
     const Server_URL = process.env.REACT_APP_SERVER_URL;
@@ -60,11 +44,7 @@ function CreatePost() {
                 onChange={e => setSummary(e.target.value)} />
             <input type="file"
                 onChange={e => setFiles(e.target.files)} />
-            <ReactQuill
-                modules={modules}
-                formats={formats}
-                value={content}
-                onChange={newValue => setContent(newValue)} />
+            <Editor value={content} onChange={setContent} />
             <button style={{ marginTop: '5px' }}>Create post</button>
         </form>
     );
